6699d0e76133819804561e7a9530b6e36c864606
DevinlearnPy <zzshuo1024@outlook.com>
2019-06-26 12:30:27 +0800

    use RandomUserAgentMiddleware

--- lvmama/settings.py
+++ lvmama/settings.py
@@ -41,17 +41,17 @@ ROBOTSTXT_OBEY = False
 #telnetconsole_enabled = false
 
 # override the default request headers:
-DEFAULT_REQUEST_HEADERS = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Language': 'en',
-	'Access-Control-Allow-Origin': '*',
-	'Cache-Control': 'no-cache, no-store, must-revalidate',
-	'Connection': 'keep-alive',
-	'Content-Encoding': 'gzip',
-	'Content-Type': 'application/javascript; charset=utf-8',
-	'Referer':'http://s.lvmama.com',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
-}
+# DEFAULT_REQUEST_HEADERS = {
+# 	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+# 	'Accept-Language': 'en',
+# 	'Access-Control-Allow-Origin': '*',
+# 	'Cache-Control': 'no-cache, no-store, must-revalidate',
+# 	'Connection': 'keep-alive',
+# 	'Content-Encoding': 'gzip',
+# 	'Content-Type': 'application/javascript; charset=utf-8',
+# 	'Referer':'http://s.lvmama.com',
+# 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+# }
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
@@ -61,10 +61,12 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-   #'lvmama.middlewares.LvmamaDownloaderMiddleware': 543,
-	# 'lvmama.middlewares.lvmaSpiderHttpProxyMiddleware':400
-# }
+DOWNLOADER_MIDDLEWARES = {
+   # 'lvmama.middlewares.LvmamaDownloaderMiddleware': 543,
+	# 'lvmama.middlewares.lvmaSpiderHttpProxyMiddleware':400,
+	'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+	'lvmama.middlewares.RandomUserAgentMiddleware':400,
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
